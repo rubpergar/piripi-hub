f21cfca631acce58c9db5e827117c9c1b5c84f3b
Sonia María Rus Morales <sonrusmor@alum.us.es>
2024-11-13 10:45:30 +0100

    feat: add new properties in services

--- app/modules/explore/services.py
+++ app/modules/explore/services.py
@@ -6,5 +6,5 @@ class ExploreService(BaseService):
     def __init__(self):
         super().__init__(ExploreRepository())
 
-    def filter(self, query="", sorting="newest", publication_type="any", tags=[], **kwargs):
-        return self.repository.filter(query, sorting, publication_type, tags, **kwargs)
+    def filter(self, query="", sorting="newest", publication_type="any", tags=[], number_of_features= "", number_of_models= "", **kwargs):
+        return self.repository.filter(query, sorting, publication_type, tags, number_of_features, number_of_models, **kwargs)
