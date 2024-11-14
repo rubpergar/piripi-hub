00e69eafd341bb324cab30c7e4497eb6b30986a5
Julia Sanchez <julia.s.marquez@gmail.com>
2024-11-14 14:55:58 +0100

    fix: verify .git/hooks/ exists in your repository

--- scripts/git-hooks/install-hooks.sh
+++ scripts/git-hooks/install-hooks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ ! -d ".git/hooks" ]; then
+  echo "Error: El directorio .git/hooks no existe. Asegúrate de estar en un repositorio Git."
+  exit 1
+fi
+
 cp scripts/git-hooks/* .git/hooks/
 chmod +x .git/hooks/*
 echo "Git hooks instalados correctamente."
