b8e57e658abe36d73fd1ecc6813daf3aa1b55bb7
Rubén Pérez Garrido <rupg29@gmail.com>
2024-12-11 10:55:52 +0000

    test: verify witch window solution fixes selenium test problem

--- app/modules/hubfile/tests/test_selenium_search_queries.py
+++ app/modules/hubfile/tests/test_selenium_search_queries.py
@@ -11,11 +11,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
+# Test 1: Usar maximizar ventana
 class TestDownloadselected1():
     """Prueba que descarga 1 modelo seleccionado"""
 
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
+        self.driver.maximize_window()  # Maximizar ventana
         self.vars = {}
 
     def teardown_method(self, method):
@@ -23,12 +25,12 @@ class TestDownloadselected1():
 
     def test_downloadselected1(self):
         self.driver.get("http://127.0.0.1:5000/")
-        self.driver.set_window_size(1214, 768)
         self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
         self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(3) .model-checkbox").click()
         self.driver.find_element(By.ID, "downloadSelectedButton").click()
 
 
+# Test 2: Usar desplazamiento (scrolling)
 class TestDownloadselected2():
     """Prueba que descarga 2 modelos seleccionados diferentes"""
 
@@ -42,17 +44,23 @@ class TestDownloadselected2():
     def test_downloadselected2(self):
         self.driver.get("http://127.0.0.1:5000/")
         self.driver.set_window_size(1214, 768)
-        self.driver.find_element(By.LINK_TEXT, "Sample dataset 3").click()
+
+        element = self.driver.find_element(By.LINK_TEXT, "Sample dataset 3")
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)  # Desplazamiento
+        element.click()
+
         self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(2) .model-checkbox").click()
         self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(4) .model-checkbox").click()
         self.driver.find_element(By.ID, "downloadSelectedButton").click()
 
 
+# Test 3: Usar un tamaño de ventana específico
 class TestDownloadselected0():
-    """Prueba que descarga 0 modelos seleccionado"""
+    """Prueba que descarga 0 modelos seleccionados"""
 
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
+        self.driver.set_window_size(1920, 1080)  # Tamaño específico
         self.vars = {}
 
     def teardown_method(self, method):
@@ -60,7 +68,7 @@ class TestDownloadselected0():
 
     def test_downloadselected0(self):
         self.driver.get("http://127.0.0.1:5000/")
-        self.driver.set_window_size(1214, 768)
         self.driver.find_element(By.LINK_TEXT, "Sample dataset 1").click()
         self.driver.find_element(By.ID, "downloadSelectedButton").click()
-        assert self.driver.switch_to.alert.text == "No files selected for download."
+        alert_text = self.driver.switch_to.alert.text
+        assert alert_text == "No files selected for download."
